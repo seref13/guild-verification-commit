4b4cd67c5dbd448487089adf77956b1a4bf409c0
şeref çelikel <36123850+seref13@users.noreply.github.com>
2026-03-05 15:52:41 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:52:28.388Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T12:52:40.253Z
+Commit: Fix memory leak in event handlers

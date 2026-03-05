ab17b064ac5945246c3a63bdbda462f3e8d22e5b
şeref çelikel <36123850+seref13@users.noreply.github.com>
2026-03-05 15:52:29 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:52:26.656Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T12:52:28.388Z
+Commit: Add export functionality for reports

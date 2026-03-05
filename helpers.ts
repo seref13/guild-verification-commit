a2d85e9ca797234d62b948e59bf45d922f5490cb
şeref çelikel <36123850+seref13@users.noreply.github.com>
2026-03-05 15:54:27 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:54:11.951Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T12:54:26.970Z
+Commit: Resolve timezone issues in date handling

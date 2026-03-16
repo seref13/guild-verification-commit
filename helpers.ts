486a6e64bf0efa5646e60bd525eb23cfbe445c70
şeref çelikel <36123850+seref13@users.noreply.github.com>
2026-03-16 21:29:33 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T18:29:29.591Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-16T18:29:32.108Z
+Commit: Patch security vulnerability in authentication

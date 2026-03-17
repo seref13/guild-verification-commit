20a17f7f396f1b91267181411f08c41c60587a1d
şeref çelikel <36123850+seref13@users.noreply.github.com>
2026-03-17 12:25:28 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-17T09:25:22.523Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-17T09:25:27.457Z
+Commit: Fix CSS styling bug on mobile devices
